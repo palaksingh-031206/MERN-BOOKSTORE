e34f15cc440604674de6ef175975a863d86a5218
Palak Singh <chmcs2026.palak.singh@gmail.com>
2026-02-20 09:40:22 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const api=axios.create({
-    baseURL:'http://localhost:3000'
+    //baseURL:'http://localhost:3000'
+    baseURL:'https://mern-bookstore-1ziu.onrender.com/'
 })
 
 export default api
